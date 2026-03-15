cee2f2693e69accb1d1e5f04c9aa85183bc50503
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2026-03-16 00:04:34 +0900

    0.0.27_1
    Resource Generator
    - additional settings for generating cool time, rate, and step amount

--- Assets/Scripts/_GamePlay/_Environment/_Events/Resource_Generator.cs
+++ Assets/Scripts/_GamePlay/_Environment/_Events/Resource_Generator.cs
@@ -13,6 +13,12 @@ public class ResourceGenerate_Data
     public TileScrObj[] generateTiles => _generateTiles;
 
     [Space(10)]
+    [SerializeField][Range(0, 100)] private int _generateRate;
+    public int generateRate => _generateRate;
+
+    [SerializeField][Range(0, 100)] private int _generateAmount;
+    public int generateAmount => _generateAmount;
+
     [SerializeField][Range(0, 100)] private int _maxGenerateAmount;
     public int maxGenerateAmount => _maxGenerateAmount;
 
@@ -34,6 +40,11 @@ public class ResourceGenerate_Data
 public class Resource_Generator : MonoBehaviour
 {
     [Space(20)]
+    [SerializeField][Range(0, 10)] private int _generateCoolTime;
+
+    private int _currentCoolTime;
+
+    [Space(10)]
     [SerializeField] private ResourceGenerate_Data[] _generateDatas;
 
 
@@ -42,12 +53,12 @@ public class Resource_Generator : MonoBehaviour
     {
         InGame_Manager.instance.time.OnTimeCount += Generate;
     }
-    
+
     private void OnDestroy()
     {
         InGame_Manager.instance.time.OnTimeCount -= Generate;
     }
-    
+
 
     // Generate
     private ResourceGenerate_Data GenerateData()
@@ -56,18 +67,18 @@ public class Resource_Generator : MonoBehaviour
         int totalWeight = 0;
         foreach (ResourceGenerate_Data data in _generateDatas)
         {
-            totalWeight += data.maxGenerateAmount;
+            totalWeight += data.generateRate;
         }
 
         // track values
         int randValue = Random.Range(0, totalWeight);
         int cumulativeWeight = 0;
 
-        // get random food according to weight
+        // get random according to weight
         for (int i = 0; i < _generateDatas.Length; i++)
         {
             ResourceGenerate_Data data = _generateDatas[i];
-            cumulativeWeight += data.maxGenerateAmount;
+            cumulativeWeight += data.generateRate;
 
             if (randValue >= cumulativeWeight) continue;
             return data;
@@ -79,7 +90,7 @@ public class Resource_Generator : MonoBehaviour
     private bool Generate_Available(Tile generateTile, Tile playerTile, ResourceGenerate_Data generateData)
     {
         float checkDistance = Utility.Chebyshev_Distance(generateTile.transform.position, playerTile.transform.position);
-        
+
         if (checkDistance < generateData.generatePointDistance) return false;
         if (!generateData.TargetTile_Match(generateTile.data.tileScrObj)) return false;
         if (generateTile.ItemPlace_AvailableCount(generateData.resourceItem) <= 0) return false;
@@ -89,6 +100,9 @@ public class Resource_Generator : MonoBehaviour
 
     private void Generate()
     {
+        _currentCoolTime++;
+        if (_currentCoolTime <= _generateCoolTime) return;
+
         InGame_Manager manager = InGame_Manager.instance;
 
         ResourceGenerate_Data generateData = GenerateData();
@@ -99,13 +113,23 @@ public class Resource_Generator : MonoBehaviour
         Tile playerTile = manager.player.movement.currentTile;
         List<Tile> tiles = new(manager.tilesController.currentTiles);
 
-        for (int i = tiles.Count - 1; i >= 0 ; i--)
+        for (int i = tiles.Count - 1; i >= 0; i--)
         {
             if (Generate_Available(tiles[i], playerTile, generateData)) continue;
             tiles.RemoveAt(i);
         }
 
-        if (tiles.Count <= 0) return;
-        tiles[Random.Range(0, tiles.Count)].Set_PlacingItem(new(generateItem, 1));
+        for (int i = 0; i < generateData.generateAmount; i++)
+        {
+            if (tiles.Count <= 0) return;
+
+            Tile generateTile = tiles[Random.Range(0, tiles.Count)];
+            generateTile.Set_PlacingItem(new(generateItem, 1));
+
+            if (generateTile.ItemPlace_AvailableCount(generateItem) > 0) continue;
+            tiles.Remove(generateTile);
+        }
+
+        _currentCoolTime = 0;
     }
 }
