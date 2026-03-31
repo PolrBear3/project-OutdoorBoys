2b94badc75c14a17c4de5240aa5feaeb910ac51d
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2026-03-31 12:00:10 +0900

    0.0.38
    Warp Renderer
    - gradual load system in progress

--- Assets/Scripts/_Systems/_Shader Material/WarpRenderer_Controller.cs
+++ Assets/Scripts/_Systems/_Shader Material/WarpRenderer_Controller.cs
@@ -30,7 +30,9 @@ public class WarpRenderer_Controller : MonoBehaviour
     [Space(20)]
     [SerializeField] private WarpRenderer_Data _defaultData;
 
+
     private MaterialPropertyBlock _materialBlock;
+    private Coroutine _loadCoroutine;
 
 
     // MonoBehaviour
@@ -55,6 +57,56 @@ public class WarpRenderer_Controller : MonoBehaviour
         _materialBlock.SetColor("_ColorC", loadData.colorC);
 
         _renderer.SetPropertyBlock(_materialBlock);
+    }
+
+    public void Load_Renderer(WarpRenderer_Data loadData, float loadDuration)
+    {
+        if (loadData == null) return;
+
+        if (_loadCoroutine != null)
+        {
+            StopCoroutine(_loadCoroutine);
+            _loadCoroutine = null;
+        }
+        _loadCoroutine = StartCoroutine(Renderer_LoadUpdate(loadData, loadDuration));
+    }
+    private IEnumerator Renderer_LoadUpdate(WarpRenderer_Data loadData, float loadDuration)
+    {
+        float startPixel = _materialBlock.GetFloat("_PixelSize");
+        float startSpeed = _materialBlock.GetFloat("_Speed");
+
+        Color startA = _materialBlock.GetColor("_ColorA");
+        Color startB = _materialBlock.GetColor("_ColorB");
+        Color startC = _materialBlock.GetColor("_ColorC");
+
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime / loadDuration;
+
+            float pixelSize = Mathf.Lerp(startPixel, loadData.pixelSize, time);
+            float animSpeed = Mathf.Lerp(startSpeed, loadData.animationSpeed, time);
+
+            Color colorA = Color.Lerp(startA, loadData.colorA, time);
+            Color colorB = Color.Lerp(startB, loadData.colorB, time);
+            Color colorC = Color.Lerp(startC, loadData.colorC, time);
+
+            _renderer.GetPropertyBlock(_materialBlock);
+
+            _materialBlock.SetFloat("_PixelSize", pixelSize);
+            _materialBlock.SetFloat("_Speed", animSpeed);
+
+            _materialBlock.SetColor("_ColorA", colorA);
+            _materialBlock.SetColor("_ColorB", colorB);
+            _materialBlock.SetColor("_ColorC", colorC);
+
+            _renderer.SetPropertyBlock(_materialBlock);
+
+            yield return null;
+        }
+
+        _loadCoroutine = null;
+        yield break;
     }
 
     private void Load_Renderer()
